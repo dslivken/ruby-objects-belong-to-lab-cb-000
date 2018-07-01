f419483cdda54f5440428086934cc2c58793eefd
David Slivken <slivken@gmail.com>
2018-07-01 12:57:31 +0000

    automatically backed up by learn

---
+++ tester.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Song
+  attr_accessor :title, :artist
+
+  def initialize (title)
+    @title = title
+  end
+
+end
+
+class Artist
+  attr_accessor :name
+
+  def initialize (name)
+    @name = name
+  end
+
+end
+
+my_song=Song.new("My Song Name")
+david=Artist.new("Slivken")
+
+binding pry
